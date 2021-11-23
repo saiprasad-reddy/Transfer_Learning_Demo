7fa038ec2999d386a337a401071ed5e2840f1be7
Saiprasad Reddy <saiprasadreddybussa@gmail.com>
2021-11-23 18:07:20 +0530

    new files created

---
+++ src/03_transfer_learning_greater_less_than_5.py
@@ -0,0 +1,155 @@
+import argparse
+import os
+import numpy as np
+import tensorflow as tf
+import shutil
+from tqdm import tqdm
+import logging
+from src.utils.common import read_yaml, create_directories
+import random
+import io
+
+
+STAGE = "Transfer Learning" ## <<< change stage name 
+
+logging.basicConfig(
+    filename=os.path.join("logs", 'running_logs.log'), 
+    level=logging.INFO, 
+    format="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s",
+    filemode="a"
+    )
+def update_greater_than_less_than_5(list_of_labels):
+    for idx, label in enumerate(list_of_labels):
+        greater_than_less_than_5_condition = label>5
+        list_of_labels[idx] = np.where(greater_than_less_than_5_condition, 1, 0)
+    return list_of_labels
+
+def main(config_path, params_path):
+    ## read config files
+    config = read_yaml(config_path)
+    
+    ##get the data
+    (X_train_full, y_train_full), (X_test, y_test) = tf.keras.datasets.mnist.load_data()
+    X_train_full = X_train_full / 255.0
+    X_test = X_test / 255.0
+    X_valid, X_train = X_train_full[:5000], X_train_full[5000:]
+    y_valid, y_train = y_train_full[:5000], y_train_full[5000:]
+
+    y_train_bin, y_test_bin, y_valid_bin = update_greater_than_less_than_5([y_train, y_test, y_valid])
+
+    ##set seeds
+    seed = 2021 ## get it from config
+    tf.random.set_seed(seed)
+    np.random.seed(seed)
+
+    ## log our model summary information in logs
+    def _log_model_summary(model):
+        with io.StringIO() as stream:
+            model.summary(print_fn= lambda x: stream.write(f"{x}\n"))
+            summary_str = stream.getvalue()
+        return summary_str
+
+    ## load the base model - 
+    base_model_path = os.path.join("artifacts", "models", "base_model.h5")
+    base_model = tf.keras.models.load_model(base_model_path)
+    logging.info(f"loaded base model summary: \n{_log_model_summary(base_model)}")
+
+    ##freeze the weights
+    for layer in base_model.layers[: -1]:
+        print(f"trainable status before {layer.name}:{layer.trainable}")
+        layer.trainable = False
+        print(f"trainable status after {layer.name}:{layer.trainable}")
+
+    base_layer = base_model.layers[: -1]
+    ##define the model and compile it
+    new_model1 = tf.keras.models.Sequential(base_layer)
+    new_model1.add(
+        tf.keras.layers.Dense(2, activation="softmax", name="output_layer")
+    )
+
+    logging.info(f"{STAGE} model summary: \n{_log_model_summary(new_model1)}")
+
+    LOSS = "sparse_categorical_crossentropy"
+    OPTIMIZER = tf.keras.optimizers.SGD(learning_rate=1e-3)
+    METRICS = ["accuracy"]
+
+    new_model1.compile(loss=LOSS, optimizer=OPTIMIZER, metrics=METRICS) 
+
+
+    ## Train the model
+    history = new_model1.fit(
+        X_train, y_train_bin, # << y_train_bin for our usecase
+        epochs=10, 
+        validation_data=(X_valid, y_valid_bin), # << y_valid_bin for our usecase
+        verbose=2)
+
+    ## save the base model - 
+    model_dir_path = os.path.join("artifacts","models")
+    model_file_path = os.path.join(model_dir_path, "even_odd_model.h5")
+    new_model1.save(model_file_path)
+
+    logging.info(f"base model is saved at {model_file_path}")
+    logging.info(f"evaluation metrics {new_model1.evaluate(X_test, y_test_bin)}")  # << y_test_bin for our usecase
+
+
+    # ##define layer
+    # LAYERS = [
+    #     tf.keras.layers.Flatten(input_shape=[28,28], name="inputlayer"),
+    #     tf.keras.layers.Dense(300, name="hiddenlayer1"),
+    #     tf.keras.layers.LeakyReLU(), ## alternative way
+    #     tf.keras.layers.Dense(100, name="hiddenlayer2"),
+    #     tf.keras.layers.LeakyReLU(),
+    #     tf.keras.layers.Dense(10,activation="softmax", name="outputlayer")
+    # ]
+
+    # ##define the model and compile it
+    # model = tf.keras.models.Sequential(LAYERS)
+
+    # LOSS = "sparse_categorical_crossentropy"
+    # OPTIMIZER = tf.keras.optimizers.SGD(learning_rate=1e-3) ##OPTIMIZER = "SGD"
+    # METRICS = ["accuracy"]
+
+    # model.compile(loss=LOSS, optimizer=OPTIMIZER, metrics=METRICS) 
+
+    # ##log our model summary information in logs
+    # def _log_model_summary(model):
+    #     with io.StringIO() as stream:
+    #         model.summary(print_fn= lambda x: stream.write(f"{x}\n"))
+    #         summary_str = stream.getvalue()
+    #     return summary_str
+    
+    # #model.summary()
+    # logging.info(f"{STAGE} model summary: \n{_log_model_summary(model)}")
+
+    # ##train the model
+    # history = model.fit(
+    #     X_train, y_train, 
+    #     epochs=10, 
+    #     validation_data=(X_valid, y_valid),
+    #     verbose=2)
+
+    # ##save the model
+    # model_dir_path = os.path.join("artifacts", "models")
+    # create_directories([model_dir_path])
+
+    # model_file_path = os.path.join(model_dir_path, "base_model.h5")
+    # model.save(model_file_path)
+
+    # logging.info(f"base model is saved at {model_file_path}")
+    # logging.info(f"evaluation metrics {model.evaluate(X_test, y_test)}")
+
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", "-c", default="configs/config.yaml")
+    args.add_argument("--params", "-p", default="params.yaml")
+    parsed_args = args.parse_args()
+
+    try:
+        logging.info("\n********************")
+        logging.info(f">>>>> stage {STAGE} started <<<<<")
+        main(config_path=parsed_args.config, params_path=parsed_args.params)
+        logging.info(f">>>>> stage {STAGE} completed!<<<<<\n")
+    except Exception as e:
+        logging.exception(e)
+        raise e
